8e47b2399310285d9a494463df057fd8026facea
MeeraEldho <meeraeldho11@gmail.com>
2024-11-25 19:55:27 +0530

    Create 3.js
    hj

---
+++ 3.js
@@ -0,0 +1,5 @@
+const isSameTree = function(p, q) {
+  if(p == null || q == null) return p === q
+  if(p.val !== q.val) return false
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
